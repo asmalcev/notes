4ba1ac45d6245aa2c0cfec2c87d851ffc82b13d8
qzerrty <qzerrty@protonmail.com>
2024-10-13 13:51:56 +0300

    feat: remove old posts, updated metadata and added including into base layout

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
 	title: "Notes",
-	url: "https://asmalcev.github.io",
+	url: "https://asmalcev.github.io/notes",
 	language: "ru",
 	description: "",
 	author: {
 		name: "Александр Мальцев",
 		email: "i@asmalcev.ru",
-		url: "https://asmalcev.github.io"
+		url: "https://asmalcev.github.io/notes"
 	}
 }
---
+++ content/about/blog.md
@@ -0,0 +1,15 @@
+---
+layout: layouts/base.njk
+eleventyNavigation:
+  key: О блоге
+  order: 4
+
+title: О блоге
+date: 2024-10-13
+description: Отчет по практической работе для ВУЗа
+---
+
+# О блоге
+
+
+
